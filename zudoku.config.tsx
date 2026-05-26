6aefefc6c106c9007f8427c42b93050c8acf6a4c
networkydev <140664254+networkydev@users.noreply.github.com>
2026-05-26 18:51:55 -0400

    chore(config): remove bloat

--- zudoku.config.tsx
+++ zudoku.config.tsx
@@ -4,57 +4,12 @@ const config: ZudokuConfig = {
   site: {
     logo: {
       src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
-      alt: "Zudoku",
+      alt: "CourseScheduler",
       width: "130px",
     },
     showPoweredBy: false,
   },
-  navigation: [
-    {
-      type: "category",
-      label: "Documentation",
-      items: [
-        {
-          type: "category",
-          label: "Getting Started",
-          icon: "sparkles",
-          items: [
-            "/introduction",
-            {
-              type: "link",
-              icon: "folder-cog",
-              badge: {
-                label: "New",
-                color: "purple",
-              },
-              label: "API Reference",
-              to: "/api",
-            },
-          ],
-        },
-        {
-          type: "category",
-          label: "Useful Links",
-          collapsible: false,
-          icon: "link",
-          items: [
-            {
-              type: "link",
-              icon: "book",
-              label: "Zudoku Docs",
-              to: "https://zudoku.dev/docs/",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      type: "link",
-      to: "/api",
-      label: "API Reference",
-    },
-  ],
-  redirects: [{ from: "/", to: "/introduction" }],
+  redirects: [{ from: "/", to: "/api" }],
   apis: [
     {
       type: "file",
